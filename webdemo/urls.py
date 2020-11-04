71d5c291e74ebca113a5203d5efbbd71a1a67f20
soukaryag <sg4fz@virginia.edu>
2020-11-04 18:32:37 -0500

    styling post entities, abstracting post request, and async calls

--- webdemo/urls.py
+++ webdemo/urls.py
@@ -6,4 +6,5 @@ app_name = 'webdemo'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('attack_interactive', views.attack_interactive, name='attack_interactive')
 ]
--- webdemo/views.py
+++ webdemo/views.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
 from django.urls import reverse
+from django.db.models.functions import Now
 
 import textattack
 import transformers
+import uuid
 import json
 import re
 
@@ -31,17 +33,39 @@ class CustomData(forms.Form):
     model_name = forms.CharField(label='Model Name', widget=forms.Select(choices=MODEL_NAMES, attrs={'class' : 'form-control'}))
     recipe_name = forms.CharField(label='Model Name', widget=forms.Select(choices=RECIPE_NAME, attrs={'class' : 'form-control'}))
     
+def GET_USER_ID(request):
+    # user session with cookies
+    USER_ID = request.session.get("TextAttackUUID")
+    if not USER_ID:
+        temp_id = str(uuid.uuid1())
+        while AttackResult.objects.filter(session_id=temp_id):
+            temp_id = str(uuid.uuid1())
+        request.session["TextAttackUUID"] = temp_id
+        USER_ID = temp_id
+
+    return USER_ID
 
 def index(request):
+    USER_ID = GET_USER_ID(request)
+    form = CustomData()
+    posts = AttackResult.objects.filter(session_id=USER_ID).order_by('-timestamp')[:10]
+
+    return render(request, 'webdemo/index.html', {'form': form, 'posts': posts})
+
+def attack_interactive(request):
     if request.method == 'POST':
+        USER_ID = GET_USER_ID(request)
         form = CustomData(request.POST)
         if form.is_valid():
             input_text, model_name, recipe_name = form.cleaned_data['input_text'], form.cleaned_data['model_name'], form.cleaned_data['recipe_name']
             if AttackResult.objects.filter(input_string=input_text, model_name=model_name, recipe_name=recipe_name).exists():
-                db_object = AttackResult.objects.get(input_string=input_text, model_name=model_name, recipe_name=recipe_name)
-                result = db_object.output_string
-                model_name = db_object.model_name
-                recipe_name = db_object.recipe_name
+                tmp = AttackResult.objects.get(input_string=input_text, model_name=model_name, recipe_name=recipe_name)
+                print("-"*100)
+                print(tmp)
+                print("-"*100)
+                if not AttackResult.objects.filter(input_string=input_text, model_name=model_name, recipe_name=recipe_name, session_id=USER_ID).exists():
+                    AttackResult.objects.update_or_create(session_id=USER_ID, input_string=tmp.input_string, model_name=tmp.model_name, recipe_name=tmp.recipe_name, output_string=tmp.output_string, input_histogram=tmp.input_histogram, output_histogram=tmp.output_histogram, input_label=tmp.input_label, output_label=tmp.output_label)
+                AttackResult.objects.filter(session_id=USER_ID, input_string=input_text, model_name=model_name, recipe_name=recipe_name).update(timestamp=Now())
             else:
                 attack = textattack.commands.attack.attack_args_helpers.parse_attack_from_args(Args(model_name, recipe_name))
                 attacked_text = textattack.shared.attacked_text.AttackedText(input_text)
@@ -54,12 +78,8 @@ def index(request):
 
                 result = next(attack.attack_dataset([(input_text, goal_func_result.output)]))
                 result_parsed = result.str_lines()
-                print("-"*100)
-                print(result_parsed)
-                print("-"*100)
                 if len(result_parsed) < 3:
-                    HttpResponseRedirect(reverse('webdemo:index'))
-                    return
+                    return HttpResponseNotFound('Failed')
                 output_text = result_parsed[2]
 
                 attacked_text_out = textattack.shared.attacked_text.AttackedText(output_text)
@@ -70,16 +90,10 @@ def index(request):
                 raw_output = [float(x) for x in list(goal_func_result.raw_output)]
                 output_histogram = json.dumps(raw_output)
 
-                AttackResult.objects.update_or_create(session_id=request.session._session_key, input_string=input_text, model_name=model_name, recipe_name=recipe_name, output_string=output_text, input_histogram=input_histogram, output_histogram=output_histogram, input_label=input_label, output_label=output_label)
+                AttackResult.objects.update_or_create(session_id=USER_ID, input_string=input_text, model_name=model_name, recipe_name=recipe_name, output_string=output_text, input_histogram=input_histogram, output_histogram=output_histogram, input_label=input_label, output_label=output_label)
         else:
-            HttpResponseRedirect(reverse('webdemo:index'))
+            return HttpResponseNotFound('Failed')
 
-        form = CustomData()
-        posts = AttackResult.objects.filter(input_string=input_text, model_name=model_name, recipe_name=recipe_name)
-        return render(request, 'webdemo/index.html', {'form': form, 'posts': posts})
-    else:
-        form = CustomData()
-        posts = AttackResult.objects.filter(session_id=request.session._session_key)[:10]
+        return HttpResponse('Success')
 
-        return render(request, 'webdemo/index.html', {'form': form, 'posts': posts})
-    
+    return HttpResponseNotFound('<h1>Not Found</h1>')
