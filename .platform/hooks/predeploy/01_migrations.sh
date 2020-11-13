9a93af6464e656db1b70593745494e3d5022fd2b
soukaryag <sg4fz@virginia.edu>
2020-11-12 23:34:05 -0500

    collectstatic first

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -3,6 +3,6 @@
 source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
