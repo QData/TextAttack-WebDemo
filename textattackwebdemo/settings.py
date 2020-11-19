f02b5c6a889db018780439b5adbda7bcf368eff9
soukaryag <sg4fz@virginia.edu>
2020-11-19 12:11:27 -0500

    collecstatic commands

--- textattackwebdemo/settings.py
+++ textattackwebdemo/settings.py
@@ -134,5 +134,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-mimetypes.add_type("text/css", ".css", True)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
