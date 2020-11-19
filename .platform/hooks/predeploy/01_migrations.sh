f869beab4beaa4f13852f58854a78fb50c2ecacd
soukaryag <sg4fz@virginia.edu>
2020-11-19 12:23:02 -0500

    tweak eb settings

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -3,6 +3,5 @@
 echo "Entering predeploy hook"
 source /var/app/venv/*/bin/activate
 cd /var/app/current
-python manage.py makemigrations
-python manage.py migrate
+
 python manage.py collectstatic --noinput
