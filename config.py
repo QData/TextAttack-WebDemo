484c8ff45556ae2914f3f81eee342502e048c2e3
Yanjun Qi <qiyanjun2012@gmail.com>
2022-03-15 10:14:37 -0400

    archive simple streamlit versino to a branch

--- config.py
+++ config.py
@@ -6,4 +6,3 @@ ATTACK_RECIPES =  textattack.commands.attack.attack_args.ATTACK_RECIPE_NAMES
 HIDDEN_ATTACK_RECIPES = ['alzantot', 'seq2sick', 'hotflip']
 
 PRECOMPUTED_RESULTS_DICT_NAME = 'results.p'
-HISTORY = 'timeline_history'
