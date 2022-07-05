3df83659e444d78fa22c5a67deadfa649b9af94f
gmurro <g.murro23@gmail.com>
2022-07-05 17:46:10 +0200

    Change old arg reference

--- webdemo/config.py
+++ webdemo/config.py
@@ -1,5 +1,5 @@
 import textattack
 
-MODELS = textattack.commands.attack.attack_args.HUGGINGFACE_DATASET_BY_MODEL
-ATTACK_RECIPES =  textattack.commands.attack.attack_args.ATTACK_RECIPE_NAMES
+MODELS = textattack.dataset_args.HUGGINGFACE_DATASET_BY_MODEL
+ATTACK_RECIPES =  textattack.attack_args.ATTACK_RECIPE_NAMES
 HIDDEN_ATTACK_RECIPES = ['alzantot', 'seq2sick', 'hotflip']
